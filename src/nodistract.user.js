5a5157aad178ea58ca4cee4ac9d8f88fb08aba92
vmayorov <vmayorov@nxsystemsinc.com>
2022-01-13 18:01:15 +0300

    tested the hook

--- src/nodistract.user.js
+++ src/nodistract.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NoDistract
 // @namespace    http://tampermonkey.net/
-// @version      0.2.6
+// @version      0.2.17
 // @description  an attempt to take over the world!
 // @author       You
 // @match        https://2ch.hk/*
